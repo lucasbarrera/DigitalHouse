bf2b2f21514b48f2b516384f618b14b30fabf7dd
Lucas Barrera Menem <barreramenemlucas@gmail.com>
2024-05-09 21:29:13 -0300

    5° agrego clase10 json

--- ejerciciosPlayground/clase09-objetosymodulos/23app.js
+++ ejerciciosPlayground/clase09-objetosymodulos/23app.js
@@ -1,13 +1,9 @@
 const fs = require("fs");
 
-fs.readFile(
-  "C:/Users/Lenovo/Desktop/LUCAS/DigitalHouse/ejerciciosPlayground/clase09-objetosymodulos/22mensaje.txt",
-  "utf-8",
-  (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
+fs.readFile("./22mensaje.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
   }
-);
+  console.log(data);
+});
---
+++ ejerciciosPlayground/clase10-json/25datosBici.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+function leerArchivo() {
+  let archivo = fs.readFileSync("./24bicicletas.json", "utf8");
+  return JSON.parse(archivo);
+}
+
+console.log(leerArchivo);
+module.exports = {
+  leerArchivo,
+};
