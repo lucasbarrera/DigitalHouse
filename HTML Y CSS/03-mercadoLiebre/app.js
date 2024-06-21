29bdb164f06af97f8c4f6942b64e2c69ec74259c
Lucas Barrera Menem <barreramenemlucas@gmail.com>
2024-06-21 15:29:46 -0300

    actualizar repo 21-06

--- HTML Y CSS/03-mercadoLiebre/app.js
+++ HTML Y CSS/03-mercadoLiebre/app.js
@@ -9,6 +9,12 @@ app.use(express.static(publicPath));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
 app.listen(8000, () => {
   console.log("Servidor corriendo en puerto 8000");
 });
